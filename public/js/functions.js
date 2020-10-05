c10a7768c578cca2a6a05864112de67910c205e8
alisq <alisqadeer@web607.webfaction.com>
2020-10-05 20:50:47 +0000

    adding artist page. update artwork url

---
+++ public/js/functions.js
@@ -0,0 +1,24 @@
+
+//this generated artist-info
+function artistInfo() {
+	
+}
+
+
+//this generates related content by artist
+function related(rel,artist) {
+
+	c = ""
+    for(r=0;r<rel.length;r++) {
+    	console.log(rel[r])
+        c+=	"<div class='teaser'>"+
+        		"<a href='/agyu/public/full.html?id="+rel[r].nid+"'>"+
+        		"<img src='"+rel[r].field_images.split(",")[0]+"' />"+
+        		"<strong>"+rel[r].title+"</strong><br />"+
+        		artist+
+        	"</a>"+
+        	"</div>"
+   }
+
+    return c
+}
